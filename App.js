88bc8b22e75831ef155358f1b4ca0cfe1e90028a
Owuor Timon Odhiambo <infotechhaven6@gmail.com>
2025-06-04 20:35:14 +0300

    update

--- App.js
+++ App.js
@@ -13,11 +13,10 @@ app.use(cors());
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB Atlas');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
   })
-  .catch(err => console.error(err));
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
 
 
     
