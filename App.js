9ade261464a88d3a5e19d2d212e3c2e45a71e360
Owuor Timon Odhiambo <143827332+carlosal10@users.noreply.github.com>
2025-06-05 17:19:49 +0300

    Update App.js

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ const adminAuthRoutes = require('./routes/AdminAuth');
 const statsRoutes = require('./routes/Stats');
 const payProcessRoutes = require('./routes/PayProcess');
 const mikrotikUserRoutes = require('./routes/mikrotikUser');
-const ConnectRoutes = require('./routes/mikrotikclient');
+const mikrotikConnect = require('./routes/mikrotikConnect');
 
 app.use('/api/customers', customerRoutes);
 app.use('/api/plans', planRoutes);
@@ -53,7 +53,7 @@ app.use('/api/auth', adminAuthRoutes);
 app.use('/api', statsRoutes);
 app.use('/api/payProcess', payProcessRoutes);
 app.use('/api/mikrotik/users', mikrotikUserRoutes);
-app.use('/api/connect', ConnectRoutes);
+app.use('/api', mikrotikConnect);
 
 
 // Start Server
