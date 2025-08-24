de0c1103210cecfdc4f37a3d6170518eb16e5f2b
Timon Odhiambo <infotechhaven6@gmail.com>
2025-08-24 23:54:42 +0300

    update

--- server/App.js
+++ server/App.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000", // frontend URL
+    origin: process.env.CLIENT_URL || "https://isp-billing-1-rsla.onrender.com", // frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
