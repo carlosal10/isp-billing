1b5a06160aa28a46afdc38109bf6843923c65660
Timon Odhiambo <infotechhaven6@gmail.com>
2025-09-23 16:07:02 +0300

    Static queue ↔ payment sync, address-list updates, dashboard state, etc.

---
+++ rewrite_mbm.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+bt = chr(96)
+lines = []
+lines.append("// utils/mikrotikBandwidthManager.js")
+lines.append("// utils/mikrotikBandwidthManager.js")
+lines.append('')
+lines.append('const { sendCommand } = require('./mikrotikConnectionManager');')
+lines.append('')
